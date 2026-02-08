50d97797ad51b38fb80d732060728a6aa57f44c1
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-08 22:50:21 +0600

    added array-looping.js file

---
+++ array-looping.js
@@ -0,0 +1,18 @@
+//for of
+const friends = ['selina', 'jubaida', 'mollika', 'semolika'];
+// for(let friend of friends){
+//     console.log(friend);
+// }
+
+//plain for loop
+for(let i = 0; i < friends.length; i ++){
+    console.log(friends[i]);
+}
+
+//while loop
+ const numbers = [20, 31, 33, 66, 72];
+ let i = 0;
+ while(i < numbers.length){
+    console.log(numbers[i]);
+    i ++;
+ }
